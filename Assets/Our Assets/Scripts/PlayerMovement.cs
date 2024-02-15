b16e29e52705e3c6de407ead55742f77ab776542
RonTheDragon <gweta.ron@gmail.com>
2024-02-15 20:59:23 +0200

    Player Movement + Multiplayer

--- Assets/Our Assets/Scripts/PlayerMovement.cs
+++ Assets/Our Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed = 5f; // Adjust this value to change movement speed
+    [SerializeField] private float _rotationSpeed = 10f;
+    [SerializeField] private float _walkingRotation = 45f;
+    [SerializeField] private Transform _playerModel;
+    private CharacterController _characterController => GetComponentInChildren<CharacterController>();
+    private Vector2 _moveInput;
+
+    private void Update()
     {
-        
+        // Move the player based on input
+        Vector3 moveDirection = new Vector3(-_moveInput.x, 0f, _moveInput.y).normalized;
+        moveDirection = Quaternion.Euler(0, _walkingRotation, 0) * moveDirection; // Rotate the movement direction by 45 degrees
+        _characterController.Move(moveDirection * _moveSpeed * Time.deltaTime);
+
+        // Rotate the player model towards the walking direction
+        if (_moveInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            _playerModel.rotation = Quaternion.Slerp(_playerModel.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMoveForward(InputAction.CallbackContext context)
     {
-        
+        // Get the movement input from the Input System
+        _moveInput.x = context.ReadValue<float>();
+    }
+
+    public void OnMoveSideways(InputAction.CallbackContext context)
+    {
+        // Get the movement input from the Input System
+        _moveInput.y = context.ReadValue<float>();
     }
 }
